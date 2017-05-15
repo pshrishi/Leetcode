b3a3eb8abbee2e588d1f1c052ce25291e877d896
Girish Patil <Girish.Patil@live.in>
2017-05-14 18:18:01 -0700

    Distribute Candies

--- src/Easy/DistributeCandies.java
+++ src/Easy/DistributeCandies.java
@@ -1,10 +1,34 @@
 package Easy;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 public class DistributeCandies {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println(new DistributeCandies().distributeCandies(new int[] {1, 2, 3, 4, 2, 2}));
 	}
-
+	
+	public int distributeCandies(int[] candies) {
+        Map map = new HashMap<Integer, Integer>();
+		Set unique = new HashSet<Integer>();
+		
+        for (int i=0; i<candies.length; i++) {
+        	if(map.containsKey(candies[i])) {
+        		map.put(candies[i], map.get(candies[i] + 1));
+        	}
+        	else
+        		map.put(candies, 1);
+        	
+        	if (!unique.contains(candies[i]))
+        		unique.add(candies[i]);
+        }
+		
+        if (candies.length/2 > unique.size())
+        	return unique.size();
+        else
+        	return candies.length/2;
+    }
 }
